1ca27e9d4330ab2723bac2bde297c0ff99ba4fae
chenjiawei@zhuojianchina.com <chenjiawei@zhuojianchina.com>
2023-12-02 15:35:15 +0800

    重要的伪类before和after

---
+++ test.js
@@ -0,0 +1,3 @@
+let uri = 'http://localhost:8686/pages/futuredoctorapp/index.html/#/wxLoginByToken?goToUrl=%2Fpages%2Ffuturedoctorapp%2Findex.html%23%2Frt%2Frotation_secretary%3Fmenusid%3D2491'
+let url =  decodeURIComponent(uri)
+console.log(url)
